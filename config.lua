34cd65b4130429c2c502584e76f3033688388bf2
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2024-03-15 14:36:21 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,43 @@
+Config = {}
+
+Config = {
+    ColorMenuR = 255,
+    ColorMenuG = 0,
+    ColorMenuB = 0,
+    ColorMenuA = 80,
+    MenuPositionX = 0,
+    MenuPositionY = 0,
+    MenuName = "Administration", 
+    SubName = "Intéractions",
+
+    AdminRanks = {"helper","mod","admin","superadmin","owner","_dev"},
+
+    OpenMenu = 38, 
+    NoclipKey = "F2",
+
+    KeybindNoclip = true,
+    Warn = true,
+
+    DiscordName = "Az",
+    DiscordLogo = "",
+    DiscordWebHook = "",
+    -- rankk list : helper, mod, admin, superadmin, owner, _dev 
+    Gestionsoi = {"helper","mod","admin","superadmin","owner","_dev"},
+    Vehicules = {"helper","mod","admin","superadmin","owner","_dev"},
+    joueurs = {"helper","mod","admin","superadmin","owner","_dev"},
+    reports = {"helper","mod","admin","superadmin","owner","_dev"},
+    warn = {"helper","mod","admin","superadmin","owner","_dev"},
+    peds = {"helper","mod","admin","superadmin","owner","_dev"},
+
+    ColorCoords_R = 225,
+    ColorCoords_G = 55,
+    ColorCoords_B = 55,
+    ColorCoords_A = 255,
+
+}
+
+Config.TeleportOptions = {
+	Parking = {position = {x = 215.75, y = -788.75, z = 30.82}},
+    Comissariat = {position = {x = 414.47, y = -980.28, z = 29.45}},
+    Hopital = {position = {x = 282.57, y = -612.1, z = 43.29}},
+}
